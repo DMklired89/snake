9953aee6864dcc77c1db98edf6f160aaef87855b
DMklired89 <edrenoff@icloud.com>
2015-12-27 22:01:19 +0600

    Дабавлен конструктор, изучена инкапсуляция.

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -12,7 +12,18 @@ namespace Snake
         public int y;      // три переменные
         public char sym;   // в новый тип данных 
 
-        public void Draw()
+        public Point()     // Функция коструктор (называется как класс и не имеет возвращаемого значения) 
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)   
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }   
+        
+        public void Draw()   // Draw тоже ничего не возвращает, но это прописывается за счет void 
         {
             Console.SetCursorPosition(x, y);  // показать на экране координаты
             Console.Write(sym);               // показать на экране символ "*"
